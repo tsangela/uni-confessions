95197d951f6358ef3c1797e63ef8498bca7c943d
Angela Tsang <tsangangela_@hotmail.com>
2020-06-21 14:25:55 -0700

    add uuid and convert form to hook

--- client/src/components/form/index.js
+++ client/src/components/form/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
+import React, { useState } from 'react';
+import uuid from 'react-uuid';
+import { useDispatch } from 'react-redux';
 import { addMessage } from '../../redux/actions/messages';
 import options from '../../resources/options.json';
 
@@ -12,34 +12,30 @@ const DEFAULT_STATE = {
   text: '',
 };
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.formRef = React.createRef();
-    this.state = DEFAULT_STATE;
-  }
+const Form = () => {
+  const dispatch = useDispatch();
+  const formRef = React.useRef(null);
+  const [message, setMessage] = useState(DEFAULT_STATE);
 
-  setFormRef = (ref) => {
-    this.formRef = ref;
+  const clearForm = () => {
+    // reset state
+    setMessage(DEFAULT_STATE);
+
+    // clear form
+    formRef.current.reset();
   };
 
-  clearForm = () => {
-    this.setState(DEFAULT_STATE);
-    this.formRef.reset();
-  };
+  const handleInput = (event) => {
+    const input = { [event.target.name]: event.target.value };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
+    setMessage((prevState) => {
+      return { ...prevState, ...input };
     });
   };
 
-  handleSubmit = () => {
-    // retrieve input
-    const { username, age, university, text } = this.state;
-    const { addMessage } = this.props;
-
+  const handleSubmit = () => {
     // validate input
+    const { username, age, university, text } = message;
     if (!username || age === NONE || university === NONE || !text) {
       alert('please ensure that all fields are filled in 😊');
       return;
@@ -47,117 +43,114 @@ class Form extends React.Component {
 
     // generate date and id
     const now = new Date();
-    const id = `${username}_${now.getTime()}`;
     const date = now.toLocaleString();
+    const id = uuid();
+    // const id = `${username}_${now.getTime()}`;
 
     // construct message
-    const message = { id, date, username, age, university, text };
+    const newMessage = { id, date, username, age, university, text };
 
     // add message and clear form
-    addMessage(message);
-    this.clearForm();
+    dispatch(addMessage(newMessage));
+    clearForm();
   };
 
-  render() {
-    const { age, university } = this.state;
-    return (
-      <div id="modal-form" className="modal">
-        <h1>tell me a secret</h1>
-        <form ref={this.setFormRef} id="confession-form">
-          <div id="form-name" className="form-group">
-            <label htmlFor="username">username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="ex: anonymous"
-              onBlur={this.handleChange}
-            />
-          </div>
-          <div className="form-inline">
-            <div id="form-age" className="form-group">
-              <label htmlFor="age">age</label>
-              <select
-                id="age"
-                name="age"
-                value={age}
-                onChange={this.handleChange}
-              >
-                <option disabled value={NONE}>
-                  {NONE}
-                </option>
-                {options.ageRanges.map((age) => (
-                  <option key={age} value={age}>
-                    {age}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div id="form-university" className="form-group">
-              <label htmlFor="university">university</label>
-              <select
-                id="university"
-                name="university"
-                value={university}
-                onChange={this.handleChange}
-              >
-                <option disabled value={NONE}>
-                  {NONE}
-                </option>
-                {options.universities.map((uni) => (
-                  <option key={uni} value={uni}>
-                    {uni}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="text">confession</label>
-            <textarea
-              id="text"
-              name="text"
-              className="text-box"
-              placeholder="type here..."
-              onBlur={this.handleChange}
-            />
-          </div>
-          <div className="center-wrapper-button">
-            <div
-              id="clear-button"
-              className="form-button clear"
-              role="button"
-              aria-label="clear form"
-              tabIndex={0}
-              onClick={this.clearForm}
-              onKeyDown={this.clearForm}
-            >
-              clear
-            </div>
-            <div
-              id="post-button"
-              className="form-button post"
-              role="button"
-              aria-label="submit form"
-              tabIndex={0}
-              onClick={this.handleSubmit}
-              onKeyDown={this.handleSubmit}
-            >
-              post
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  const handleKey = (event, fn) => {
+    // enter key
+    if (event.key === 'Enter') {
+      fn();
+    }
+  };
 
-const mapStateToProps = (state) => {
-  return { messages: state.messageReducer.messages };
+  return (
+    <div id="modal-form" className="modal">
+      <h1>tell me a secret</h1>
+      <form ref={formRef} id="confession-form">
+        <div id="form-name" className="form-group">
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="ex: anonymous"
+            onBlur={handleInput}
+          />
+        </div>
+        <div className="form-inline">
+          <div id="form-age" className="form-group">
+            <label htmlFor="age">age</label>
+            <select
+              id="age"
+              name="age"
+              value={message.age}
+              onChange={handleInput}
+            >
+              <option disabled value={NONE}>
+                {NONE}
+              </option>
+              {options.ageRanges.map((age) => (
+                <option key={age} value={age}>
+                  {age}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div id="form-university" className="form-group">
+            <label htmlFor="university">university</label>
+            <select
+              id="university"
+              name="university"
+              value={message.university}
+              onChange={handleInput}
+            >
+              <option disabled value={NONE}>
+                {NONE}
+              </option>
+              {options.universities.map((uni) => (
+                <option key={uni} value={uni}>
+                  {uni}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="text">confession</label>
+          <textarea
+            id="text"
+            name="text"
+            className="text-box"
+            placeholder="type here..."
+            onBlur={handleInput}
+          />
+        </div>
+        <div className="center-wrapper-button">
+          <div
+            id="clear-button"
+            className="form-button clear"
+            role="button"
+            aria-label="clear form"
+            tabIndex={0}
+            onClick={clearForm}
+            onKeyDown={(event) => handleKey(event, clearForm)}
+          >
+            clear
+          </div>
+          <div
+            id="post-button"
+            className="form-button post"
+            role="button"
+            aria-label="submit form"
+            tabIndex={0}
+            onClick={handleSubmit}
+            onKeyDown={(event) => handleKey(event, handleSubmit)}
+          >
+            post
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, { addMessage })(Form);
-
-Form.propTypes = {
-  addMessage: PropTypes.func.isRequired,
-};
+export default Form;
